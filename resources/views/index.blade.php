d460c0deeecc045dd3794c0437c8c03ec5b293d3
Tobias Kronthaler <tobias.kronthaler@diemayrei.de>
2017-03-21 19:56:09 +0100

    Improved table layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <table style="width: 100%">
+    <table style="width:100%;table-layout:fixed">
         <thead>
         <tr>
             <th>When</th>
-            <th>Where</th>
-            <th>What</th>
+            <th>Boss</th>
             <th>Duration</th>
             <th>Party DPS</th>
             <th>Data</th>
@@ -16,9 +15,8 @@
         @foreach($encounters as $encounter)
             <tr>
                 <td>{{ $encounter->encounter_unix }}</td>
-                <td>{{ $encounter->getAreaName() }}</td>
-                <td>{{ $encounter->getMonsterName() }}</td>
-                <td>{{ gmdate('H:i:s', $encounter->data->fightDuration) }}</td>
+                <td>{{ $encounter->getMonsterName() }} - {{ $encounter->getAreaName() }}</td>
+                <td>{{ gmdate('i:s', $encounter->data->fightDuration) }}</td>
                 <td>{{ \App\Stat::damageFormat($encounter->data->partyDps)  }}</td>
                 <td><a href="{{ route('getStat', $encounter) }}" target="_blank">Raw</a></td>
             </tr>
