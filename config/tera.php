8e383010cb223e08e2609d152b56b7c4727fa247
Tobias Kronthaler <kronthto@gmail.com>
2017-12-29 10:41:45 +0100

    Added BP to whitelisted regions

--- config/tera.php
+++ config/tera.php
@@ -8,10 +8,13 @@ return [
         950, // Harrowhold
         794, // Thaumetal Refinery
         994, // Thaumetal Refinery (Hard)
+        716, // Sky Cruiser Endeavor
         916, // Sky Cruiser Endeavor (Hard)
         769, // Lilith's Keep
         969, // Lilith's Keep (Hard)
         460, // Kalivan's Dreadnaught (Hard)
         760, // Kalivan's Dreadnaught
+        710, // Broken Prison
+        810, // Lakan's Prison
     ],
 ];
